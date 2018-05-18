dedfdfaa26dcfa71c9f57519e3e8dfbaf5916479
ekno27 <edgar.canozelaya.6@my.csun.edu>
2018-05-17 20:09:57 -0700

    final working version'

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,16 +19,17 @@ console.log(alphabet);
  console.log(correctLetter);
 
  //variable declarations
- var wins = 0;
- var losses = 0; 
- var guessesLeft = 10;
- var lettersGuessed;
+ 
 
  //getting each element 
  var winnerText = document.getElementById("win-id");
  var lossText = document.getElementById("loss-id");
  var guessesLeftText = document.getElementById("guess-id");
  var lettersGuessedText = document.getElementById("sofar-id");
+ var wins = 0;
+ var losses = 0; 
+ var guessesLeft = 10;
+ var lettersArray=[];
  
 
  document.onkeyup = function(event){
@@ -37,20 +38,34 @@ console.log(alphabet);
 
     if(event.key ===correctLetter){
         correctLetter = alphabet[Math.ceil(Math.random() * alphabet.length)];
-        console.log(correctLetter);
+        console.log(correctLetter); //use to check for correct letter
+        alert("you win!");
         wins++;
         winnerText.innerText="Wins:" + wins ;
+        guessesLeft = 10;
+        lettersArray =[];
+        guessesLeftText.innerText="Guesses left: 10";
+        lettersGuessedText.innerText = "Your guesses so far: ";
+
         
     }
     else{
         guessesLeft--;
         guessesLeftText.innerText="Guesses left: " + guessesLeft;
-        console.log(guessesLeft);
+        lettersArray.push(input);
+        lettersGuessedText.innerText = "Your guesses so far:" + lettersArray;
         
     }
 
-    if(guessesLeft===0){
-        alert("you lose and you suck") 
+    if(guessesLeft ===0){
+        losses++;
+        lossText.innerText= "losses: " + losses;
+        alert("you lose and you suck");
+        guessesLeft = 10;
+        lettersArray =[];
+        guessesLeftText.innerText="Guesses left: 10";
+        lettersGuessedText.innerText = "Your guesses so far: ";
+
     }
 
  };
